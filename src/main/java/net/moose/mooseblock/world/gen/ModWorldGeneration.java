f502917a6d243f0f5abdaad1b04b812b83d621ab
MooseRef <wrfortier@hotmail.com>
2023-04-04 10:58:52 -0400

    Added Spawns for Entities

--- src/main/java/net/moose/mooseblock/world/gen/ModWorldGeneration.java
+++ src/main/java/net/moose/mooseblock/world/gen/ModWorldGeneration.java
@@ -2,8 +2,9 @@ package net.moose.mooseblock.world.gen;
 
 public class ModWorldGeneration {
     public static void  generateModWorldGen(){
-        ModTreeGeneration.generateTrees();
+        ModEntityGeneration.addSpawns();
         ModOreGeneration.generateOres();
+        ModTreeGeneration.generateTrees();
     }
 }
 
